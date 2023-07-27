54a2ad47282dc3ad89bbae3cb00ab57bdce25a5d
KevFan <chfan@redhat.com>
2023-07-27 12:58:04 +0100

    dep: use ginkgo v2

--- pkg/reconcilers/deployment_test.go
+++ pkg/reconcilers/deployment_test.go
@@ -1,11 +1,14 @@
 package reconcilers_test
 
 import (
+	"testing"
+
 	"github.com/kuadrant/limitador-operator/pkg/reconcilers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -87,3 +90,73 @@ var _ = Describe("Deployment", func() {
 		})
 	})
 })
+
+func TestDeploymentResourcesMutator(t *testing.T) {
+	deploymentFactory := func(requirements corev1.ResourceRequirements) *appsv1.Deployment {
+		return &appsv1.Deployment{
+			Spec: appsv1.DeploymentSpec{
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Resources: requirements,
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+
+	requirementsFactory := func(reqCPU, reqMem, limCPU, limMem string) corev1.ResourceRequirements {
+		return corev1.ResourceRequirements{
+			Requests: corev1.ResourceList{
+				corev1.ResourceCPU:    resource.MustParse(reqCPU),
+				corev1.ResourceMemory: resource.MustParse(reqMem),
+			},
+			Limits: corev1.ResourceList{
+				corev1.ResourceCPU:    resource.MustParse(limCPU),
+				corev1.ResourceMemory: resource.MustParse(limMem),
+			},
+		}
+	}
+
+	requirementsA := requirementsFactory("1m", "1Mi", "2m", "2Mi")
+	requirementsB := requirementsFactory("2m", "2Mi", "4m", "4Mi")
+
+	type args struct {
+		desired  *appsv1.Deployment
+		existing *appsv1.Deployment
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test false when desired and existing are the same",
+			args: args{
+				desired:  deploymentFactory(requirementsA),
+				existing: deploymentFactory(requirementsA),
+			},
+			want: false,
+		},
+		{
+			name: "test true when desired and existing are different",
+			args: args{
+				desired:  deploymentFactory(requirementsA),
+				existing: deploymentFactory(requirementsB),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DeploymentResourcesMutator(tt.args.desired, tt.args.existing); got != tt.want {
+				t.Errorf("DeploymentResourcesMutator() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
