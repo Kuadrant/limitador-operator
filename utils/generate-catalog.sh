81e8e008455f8c917c05fb4fe74ca4ab00fc3278
dd di cesare <didi@posteo.net>
2023-09-11 12:29:55 +0200

    [catalog] Adding channels to the catalog generation files
    * Right now it works only with one channel

--- utils/generate-catalog.sh
+++ utils/generate-catalog.sh
@@ -6,13 +6,14 @@ set -euo pipefail
 
 ### CONSTANTS
 # Used as well in the subscription object
-CHANNEL_NAME=preview
+DEFAULT_CHANNEL=preview
 ###
 
 OPM="${1?:Error \$OPM not set. Bye}"
 YQ="${2?:Error \$YQ not set. Bye}"
 BUNDLE_IMG="${3?:Error \$BUNDLE_IMG not set. Bye}"
-CATALOG_FILE="${4?:Error \$CATALOG_FILE not set. Bye}"
+CHANNELS="${4:-$DEFAULT_CHANNEL}"
+CATALOG_FILE="${5?:Error \$CATALOG_FILE not set. Bye}"
 
 CATALOG_FILE_BASEDIR="$( cd "$( dirname "$(realpath ${CATALOG_FILE})" )" && pwd )"
 CATALOG_BASEDIR="$( cd "$( dirname "$(realpath ${CATALOG_FILE_BASEDIR})" )" && pwd )"
@@ -28,11 +29,12 @@ touch ${CATALOG_FILE}
 # Limitador Operator
 ###
 # Add the package
-${OPM} init limitador-operator --default-channel=${CHANNEL_NAME} --output yaml >> ${CATALOG_FILE}
+${OPM} init limitador-operator --default-channel=${CHANNELS} --output yaml >> ${CATALOG_FILE}
 # Add a bundles to the Catalog
 cat ${TMP_DIR}/limitador-operator-bundle.yaml >> ${CATALOG_FILE}
 # Add a channel entry for the bundle
 V=`${YQ} eval '.name' ${TMP_DIR}/limitador-operator-bundle.yaml` \
-    ${YQ} eval '(.entries[0].name = strenv(V))' ${CATALOG_BASEDIR}/limitador-operator-channel-entry.yaml >> ${CATALOG_FILE}
+CHANNELS=${CHANNELS} \
+    ${YQ} eval '(.entries[0].name = strenv(V)) | (.name = strenv(CHANNELS))' ${CATALOG_BASEDIR}/limitador-operator-channel-entry.yaml >> ${CATALOG_FILE}
 
 rm -rf $TMP_DIR
