49f5a1f4f3faf49e8dbe7b48c75938e0806e7367
Eguzki Astiz Lezaun <eastizle@redhat.com>
2023-09-12 19:01:30 +0200

    pkg/reconcilers/deployment_test.go: volume and volumemount mutator test

--- pkg/reconcilers/deployment_test.go
+++ pkg/reconcilers/deployment_test.go
@@ -3,7 +3,6 @@ package reconcilers_test
 import (
 	"testing"
 
-	"github.com/kuadrant/limitador-operator/pkg/reconcilers"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"gotest.tools/assert"
@@ -11,6 +10,8 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/kuadrant/limitador-operator/pkg/reconcilers"
 )
 
 var _ = Describe("Deployment", func() {
@@ -132,5 +133,103 @@ func TestDeploymentResourcesMutator(t *testing.T) {
 
 	t.Run("test true when desired and existing are different", func(subT *testing.T) {
 		assert.Equal(subT, reconcilers.DeploymentResourcesMutator(deploymentFactory(requirementsA), deploymentFactory(requirementsB)), true)
+	})
+}
+
+func TestDeploymentVolumesMutator(t *testing.T) {
+	deploymentFactory := func(volumes []corev1.Volume) *appsv1.Deployment {
+		return &appsv1.Deployment{
+			Spec: appsv1.DeploymentSpec{
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{},
+					Spec: corev1.PodSpec{
+						Volumes: volumes,
+					},
+				},
+			},
+		}
+	}
+
+	existing := deploymentFactory([]corev1.Volume{
+		{
+			Name: "A",
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "secretA",
+					},
+				},
+			},
+		},
+	})
+
+	desired := deploymentFactory([]corev1.Volume{
+		{
+			Name: "B",
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "secretB",
+					},
+				},
+			},
+		},
+	})
+
+	desiredCopy := desired.DeepCopyObject()
+
+	t.Run("test false when desired and existing are the same", func(subT *testing.T) {
+		assert.Equal(subT, reconcilers.DeploymentVolumesMutator(existing, existing), false)
+	})
+
+	t.Run("test true when desired and existing are different", func(subT *testing.T) {
+		assert.Equal(subT, reconcilers.DeploymentVolumesMutator(desired, existing), true)
+		assert.DeepEqual(subT, desired, desiredCopy)
+		assert.DeepEqual(subT, desired, existing)
+	})
+}
+
+func TestDeploymentVolumeMountsMutator(t *testing.T) {
+	deploymentFactory := func(volumeMounts []corev1.VolumeMount) *appsv1.Deployment {
+		return &appsv1.Deployment{
+			Spec: appsv1.DeploymentSpec{
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								VolumeMounts: volumeMounts,
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+
+	existing := deploymentFactory([]corev1.VolumeMount{
+		{
+			Name:      "A",
+			MountPath: "/path/A",
+		},
+	})
+
+	desired := deploymentFactory([]corev1.VolumeMount{
+		{
+			Name:      "B",
+			MountPath: "/path/B",
+		},
+	})
+
+	desiredCopy := desired.DeepCopyObject()
+
+	t.Run("test false when desired and existing are the same", func(subT *testing.T) {
+		assert.Equal(subT, reconcilers.DeploymentVolumeMountsMutator(existing, existing), false)
+	})
+
+	t.Run("test true when desired and existing are different", func(subT *testing.T) {
+		assert.Equal(subT, reconcilers.DeploymentVolumeMountsMutator(desired, existing), true)
+		assert.DeepEqual(subT, desired, desiredCopy)
+		assert.DeepEqual(subT, desired, existing)
 	})
 }
