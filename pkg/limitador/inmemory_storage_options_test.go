ebdafb36ccd3a2128f400c73f8c92f8132744b9c
Eguzki Astiz Lezaun <eastizle@redhat.com>
2023-09-13 16:23:58 +0200

    pkg/limitador/inmemory_storage_options_test.go: inmemory storage test

---
+++ pkg/limitador/inmemory_storage_options_test.go
@@ -0,0 +1,18 @@
+package limitador
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestInMemoryDeploymentOptions(t *testing.T) {
+	t.Run("basic inmemory deployment options", func(subT *testing.T) {
+		options, err := InMemoryDeploymentOptions()
+		assert.NilError(subT, err)
+		assert.DeepEqual(subT, options,
+			DeploymentStorageOptions{
+				Command: []string{"memory"},
+			})
+	})
+}
