e070b383c0a820497ae31da2b8b9e299ee35d341
dd di cesare <didi@posteo.net>
2024-05-29 21:24:07 +0200

    [make] Packaging, uploading and sync
    Signed-off-by: dd di cesare <didi@posteo.net>

--- make/helm.mk
+++ make/helm.mk
@@ -1,9 +1,7 @@
 ##@ Helm Charts
 
 .PHONY: helm-build
-helm-build: ## Build the helm chart from kustomize manifests
-	# Generate kustomize manifests out of code notations
-	$(OPERATOR_SDK) generate kustomize manifests -q
+helm-build: $(KUSTOMIZE) $(OPERATOR_SDK) $(YQ) manifests ## Build the helm chart from kustomize manifests
 	# Set desired operator image and related limitador image
 	V="$(RELATED_IMAGE_LIMITADOR)" $(YQ) eval '(select(.kind == "Deployment").spec.template.spec.containers[].env[] | select(.name == "RELATED_IMAGE_LIMITADOR").value) = strenv(V)' -i config/manager/manager.yaml
 	# Replace the controller image
@@ -25,3 +23,43 @@ helm-uninstall: $(HELM) ## Uninstall the helm chart
 helm-upgrade: $(HELM) ## Upgrade the helm chart
 	# Upgrade the helm chart in the cluster
 	$(HELM) upgrade limitador-operator charts/limitador-operator
+
+.PHONY: helm-package
+helm-package: $(HELM) ## Package the helm chart
+	# Package the helm chart
+	$(HELM) package charts/limitador-operator
+
+# GitHub Token with permissions to upload to the release assets
+GITHUB_TOKEN ?= <YOUR-TOKEN>
+# GitHub Release ID, to find the release_id query the GET /repos/{owner}/{repo}/releases/latest or GET /repos/{owner}/{repo}/releases endpoints
+RELEASE_ID ?= <RELEASE-ID>
+# GitHub Release Asset ID, it can be find in the output of the uploaded asset
+ASSET_ID ?= <ASSET-ID>
+# GitHub Release Asset Browser Download URL, it can be find in the output of the uploaded asset
+BROWSER_DOWNLOAD_URL ?= <BROWSER-DOWNLOAD-URL>
+ifeq (0.0.0,$(VERSION))
+CHART_VERSION = $(VERSION)-dev
+else
+CHART_VERSION = $(VERSION)
+endif
+
+.PHONY: helm-upload-package
+helm-upload-package: $(HELM) ## Upload the helm chart package to the GitHub release assets
+	curl -L -s \
+      -X POST \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer $(GITHUB_TOKEN)" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      -H "Content-Type: application/octet-stream" \
+      "https://uploads.github.com/repos/$(ORG)/$(REPO_NAME)/releases/$(RELEASE_ID)/assets?name=chart-limitador-operator-$(CHART_VERSION).tgz" \
+      --data-binary "@limitador-operator-$(CHART_VERSION).tgz"
+
+.PHONY: helm-sync-package
+helm-sync-package: $(HELM) ## Sync the helm chart package to the helm-charts repo
+	curl -L \
+	  -X POST \
+	  -H "Accept: application/vnd.github+json" \
+	  -H "Authorization: Bearer $(GITHUB_TOKEN)" \
+	  -H "X-GitHub-Api-Version: 2022-11-28" \
+	  https://api.github.com/repos/$(ORG)/$(REPO_NAME)/dispatches \
+	  -d '{"event_type":"sync-chart","client_payload":{"chart":limitador-operator,"version":"$(CHART_VERSION)", "asset_id":"$(ASSET_ID)", "browser_download_url": "$(BROWSER_DOWNLOAD_URL)"}}'
