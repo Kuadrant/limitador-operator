ea31f8b5b333981be9d81fa15ef5d5cdd876dabb
didierofrivia <4183971+didierofrivia@users.noreply.github.com>
2025-02-05 17:30:08 +0000

    Prepared release v0.13.0-alpha1
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+LIMITADOR_VERSION=1.6.0
+IMG=quay.io/kuadrant/limitador-operator:v0.13.0-alpha1
+BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.13.0-alpha1
+CATALOG_IMG=quay.io/kuadrant/limitador-operator-catalog:v0.13.0-alpha1
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=0.13.0-alpha1
