d12445513a2fd448fc088c6303e3f25c2e5cc2aa
laurafitzgerald <6498727+laurafitzgerald@users.noreply.github.com>
2025-03-25 09:52:02 +0000

    Prepared release v0.13.0
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+LIMITADOR_VERSION=2.0.0
+IMG=quay.io/kuadrant/limitador-operator:v0.13.0
+BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.13.0
+CATALOG_IMG=quay.io/kuadrant/limitador-operator-catalog:v0.13.0
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=0.13.0
