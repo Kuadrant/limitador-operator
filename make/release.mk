36fb6ad5eb854b92993920bcf7adb37fbd81653e
eguzki <881529+eguzki@users.noreply.github.com>
2025-03-27 10:53:11 +0000

    Prepared release v0.13.1
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+LIMITADOR_VERSION=1.6.0
+IMG=quay.io/kuadrant/limitador-operator:v0.13.1
+BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.13.1
+CATALOG_IMG=quay.io/kuadrant/limitador-operator-catalog:v0.13.1
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=0.13.1
