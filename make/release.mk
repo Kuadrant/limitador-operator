48d1cffaa3b82944c546db0baab80928cdf9ff25
Boomatang <10224565+Boomatang@users.noreply.github.com>
2025-04-07 15:52:05 +0000

    Prepared release v0.14.0
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ make/release.mk
@@ -0,0 +1,8 @@
+#Release default values
+LIMITADOR_VERSION=2.0.0
+IMG=quay.io/kuadrant/limitador-operator:v0.14.0
+BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.14.0
+CATALOG_IMG=quay.io/kuadrant/limitador-operator-catalog:v0.14.0
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=0.14.0
