d98486d7dac0ae34fdedc65de71a616af87db3ff
adam-cattermole <6575004+adam-cattermole@users.noreply.github.com>
2026-02-18 13:31:16 +0000

    Prepared release v0.17.1
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- make/release.mk
+++ make/release.mk
@@ -1,8 +1,8 @@
 #Release default values
 LIMITADOR_VERSION=2.3.0
-IMG=quay.io/kuadrant/limitador-operator:v0.17.0
-BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.17.0
-CATALOG_IMG=quay.io/kuadrant/limitador-operator-catalog:v0.17.0
+IMG=quay.io/kuadrant/limitador-operator:v0.17.1
+BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.17.1
+CATALOG_IMG=quay.io/kuadrant/limitador-operator-catalog:v0.17.1
 CHANNELS=alpha
 BUNDLE_CHANNELS=--channels=alpha
-VERSION=0.17.0
+VERSION=0.17.1
