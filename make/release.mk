abefe4ad85de82f51a2de1b39971064c4f59feda
Eguzki Astiz Lezaun <eastizle@redhat.com>
2026-05-21 15:05:20 +0200

    Fix release scripts to work on release branch
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- make/release.mk
+++ make/release.mk
@@ -1,8 +1,9 @@
 #Release default values
-LIMITADOR_VERSION=2.3.1
-IMG=quay.io/kuadrant/limitador-operator:v0.17.2
-BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.17.2
-CATALOG_IMG=quay.io/kuadrant/limitador-operator-catalog:v0.17.2
-CHANNELS=stable
-BUNDLE_CHANNELS=--channels=stable
-VERSION=0.17.2
+LIMITADOR_VERSION?=2.3.1
+IMAGE_TAG?=v0.17.2
+IMG?=quay.io/kuadrant/limitador-operator:$(IMAGE_TAG)
+BUNDLE_IMG?=quay.io/kuadrant/limitador-operator-bundle:$(IMAGE_TAG)
+CATALOG_IMG?=quay.io/kuadrant/limitador-operator-catalog:$(IMAGE_TAG)
+CHANNELS?=stable
+BUNDLE_CHANNELS?=--channels=stable
+VERSION?=0.17.2
