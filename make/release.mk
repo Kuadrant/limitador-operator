dbdaba3b6c13383f91dba5a9fe042d02ac6ef871
Thomas Maas <thomas@webtypes.com>
2026-07-14 20:30:38 +0200

    Bump limitador operand to v2.4.2
    Update RELATED_IMAGE_LIMITADOR from v2.4.1 to v2.4.2 across manager
    config, Helm chart, and OLM bundle. Limitador v2.4.2 includes Clippy
    lint fixes and an openssl security bump (0.10.79 → 0.10.80).
    
    Signed-off-by: Thomas Maas <thomas@webtypes.com>

--- make/release.mk
+++ make/release.mk
@@ -1,5 +1,5 @@
 #Release default values
-LIMITADOR_VERSION?=2.4.1
+LIMITADOR_VERSION?=2.4.2
 IMAGE_TAG?=v0.18.2
 IMG?=quay.io/kuadrant/limitador-operator:$(IMAGE_TAG)
 BUNDLE_IMG?=quay.io/kuadrant/limitador-operator-bundle:$(IMAGE_TAG)
