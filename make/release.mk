3ae0a8a3d404d2b5d325c6b8277dcdd70493f167
thomasmaas <54224+thomasmaas@users.noreply.github.com>
2026-08-29 07:42:35 +0000

    Prepared release v0.17.4
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- make/release.mk
+++ make/release.mk
@@ -1,9 +1,9 @@
 #Release default values
 LIMITADOR_VERSION?=2.3.2
-IMAGE_TAG?=v0.17.3
+IMAGE_TAG?=v0.17.4
 IMG?=quay.io/kuadrant/limitador-operator:$(IMAGE_TAG)
 BUNDLE_IMG?=quay.io/kuadrant/limitador-operator-bundle:$(IMAGE_TAG)
 CATALOG_IMG?=quay.io/kuadrant/limitador-operator-catalog:$(IMAGE_TAG)
 CHANNELS?=stable
 BUNDLE_CHANNELS?=--channels=stable
-VERSION?=0.17.3
+VERSION?=0.17.4
